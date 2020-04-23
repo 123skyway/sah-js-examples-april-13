b94c235729facfa461a72d2e8603e09bf2b64e16
Adam Gyulavari <adam.gyulavari@greenfoxacademy.com>
2020-04-23 16:54:50 +0200

    update second part

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ let name = 'Adam';
 console.log(name + "'s apples");
 console.log('Hello world!');
 
+$('h1').text('We are having a 10 minute break until 17:05');
+
 // This a JavaScript comment
 
 /*
